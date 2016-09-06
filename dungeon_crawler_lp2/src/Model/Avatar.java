2e978b6a852aa41c974f956b17e46cea298e8187
gustavoGalarreta <gustavo.galarreta@tektonlabs.com>
2016-09-05 19:33:54 -0500

    Update Avatar.java

--- dungeon_crawler_lp2/src/Model/Avatar.java
+++ dungeon_crawler_lp2/src/Model/Avatar.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Model;
+//package Model;
 
 /**
  *
  * @author Gustavo
  */
-public class Avatar {
+//public class Avatar {
     
-}
+//}
